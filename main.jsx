43a00d4ffe7634717985f3b1d67015efc76e8a1d
slouc66-ui <slouc66@gmail.com>
2026-07-21 23:30:09 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GymTracker from "./GymTracker.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GymTracker />
-  </React.StrictMode>
-);
